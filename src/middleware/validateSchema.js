a741ff8e6d1887faa6bafe67cf75194b2973f06d
brau25-bit <brauliochaveza@gmail.com>
2026-02-09 21:41:26 -0600

    Modificacion al schema para validar datos

--- src/middleware/validateSchema.js
+++ src/middleware/validateSchema.js
@@ -1,8 +1,8 @@
 export function validateSchema(schema){
     return (req,res,next) => {
         const validated = schema.safeParse(req.body)
-
-        if(!validated) return res.json({error: error.message.flatten()})
+        
+        if(!validated.success) return res.status(400).json({error: validated.error.flatten()})
 
         req.validated = validated.data
 
--- src/schemas/user.schema.js
+++ src/schemas/user.schema.js
@@ -2,16 +2,19 @@ import z from 'zod'
 
 export const userSchema = z.object({
     name: z.string("Campo debe de ser una cadena de texto")
+        .trim()
         .min(2, "Nombre demasiado corto")
         .max(150, "Nombre demasiado largo"),
 
     lastname: z.string("Campo debe de ser una cadena de texto")
+        .trim()
         .min(2, "Apellido demasiado corto")
-        .min(100, "Apellido demasiado largo"),
+        .max(100, "Apellido demasiado largo"),
 
     secondLastname: z.string("Campo debe de ser una cadena de texto")
+        .trim()
         .min(2, "Apellido demasiado corto")
-        .min(100, "Apellido demasiado largo"),
+        .max(100, "Apellido demasiado largo"),
     
     age: z.number("Campo debe de ser un numero")
         .min(10, "Edad fuera de los parametros")
@@ -19,26 +22,32 @@ export const userSchema = z.object({
 
     email: z.string("Campo debe de ser una cadena de texto")
         .email()
+        .trim()
         .min(2, "Email demasiado corto")
         .max(255, "Email demasiado largo"),
 
     password: z.string()
+        .trim()
         .min(2, "Contraseña debe de comtener al menos 2 caracteres")
         .max(255, "Contraseña demasiada larga"),
 
     cp: z.string("Campo debe de ser una cadena de texto")
+        .trim()
         .min(5, "Codigo postal demasiado corto")
         .max(5, "Codigo postal demasiado largo"),
 
     state: z.string("Campo debe de ser una cadena de texto")
+        .trim()
         .min(2, "Estado demasiado corto")
         .max(255, "Estado demasiado largo"),
 
     area: z.string("Campo debe de ser una cadena de texto")
+        .trim()
         .min(2, "Colonia demasiado corto")
         .max(255, "Colonia demasiado largo"),
 
     profession: z.string("Campo debe de ser una cadena de texto")
+        .trim()
         .min(2, "Profesión demasiado corta")
         .max(255, "Profesión demasiado larga")
         .optional(),
