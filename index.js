f25f86c26d80f22c7bda74c7e78d639e624cd2fe
brau25-bit <brauliochaveza@gmail.com>
2026-03-13 14:27:04 -0600

    new general route Report

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import express from "express"
 import authRouter from "./src/modules/auth/auth.router.js"
-
+import reportRouter from "./src/modules/report/report.router.js"
 const app = express()
 
 app.use(express.json())
 app.disable("x-powered-by")
 
 app.use('/auth', authRouter)
+app.use('/report', reportRouter)
 
 const port = process.env.PORT || 5000
 
