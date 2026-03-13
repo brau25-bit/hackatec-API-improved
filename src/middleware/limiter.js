868f7c0036852d91d9000dfc8f3e730e6cb74ada
brau25-bit <brauliochaveza@gmail.com>
2026-03-13 15:27:15 -0600

    fix: decrease number of attempts for login

--- src/middleware/limiter.js
+++ src/middleware/limiter.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit"
 
 export const postLimit = rateLimit({
     windowMs: 1000 * 60 * 15,
-    limit: 20,
+    limit: 6,
     standardHeaders: 'draft-8',
 	legacyHeaders: false,
 	ipv6Subnet: 56,
@@ -10,7 +10,7 @@ export const postLimit = rateLimit({
 
 export const loginLimiter = rateLimit({
     windowMs: 1000 * 60 * 15,
-    limit: 5,
+    limit: 3,
     standardHeaders: 'draft-8',
     legacyHeaders: false,
     ipv6Subnet: 56
