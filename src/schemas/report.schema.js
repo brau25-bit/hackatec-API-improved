3777aaa28acd4e6fd77b28b1f41b862e136de92e
brau25-bit <brauliochaveza@gmail.com>
2026-03-13 15:30:10 -0600

    add: data validation for reports

--- src/schemas/report.schema.js
+++ src/schemas/report.schema.js
@@ -1,8 +1,28 @@
 import z from 'zod'
-import reportRouter from '../modules/report/report.router.js'
 
 export const reportSchema = z.object({
+    title: z.string()
+        .min(2, "La longitud del titulo debe de ser menor a dos caracteres")
+        .max(100, "La longitud del titulo debe de ser mayor a cien caracteres"),
 
+    description: z.string()
+        .min(2, "La longitud de la descripcion debe de ser menor a dos caracteres")
+        .max(100, "La longitud de la descripcion debe de ser mayor a cien caracteres"),
+
+    location: z.string()
+        .min(2)
+        .max(256),
+
+    longitude: z.number()
+        .min(-180)
+        .max(180),
+
+    latitude: z.number()
+        .min(-90)
+        .max(90),
+
+    userId: z.string()
+        .min(2)
 })
 
 export const partialReportSchema = reportSchema.partial()
