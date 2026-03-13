10ea50afc6cff026added2c1ef8a71fc09e0e3fb
brau25-bit <brauliochaveza@gmail.com>
2026-03-13 15:29:40 -0600

    add: report controller and service

--- src/modules/report/report.controller.js
+++ src/modules/report/report.controller.js
@@ -5,15 +5,22 @@ export class ReportController{
         try {
 
         } catch (error) {
-            
+            throw error
         }
     }
 
     static async createReport(req, res){
         try {
-            
+            const data = req.validated
+            const imgPath = req.file
+
+            await ReportService.createReports(data, imgPath)
+
+            return res.status(200).json({
+                message: "Reporte creado exitosamente"
+            })
         } catch (error) {
-            
+            throw error
         }
     }
 }
--- src/modules/report/report.service.js
+++ src/modules/report/report.service.js
@@ -1,4 +1,4 @@
-
+import prisma from "../../db/prisma.js";
 
 export class ReportService{
     static async getReports(){
@@ -9,9 +9,29 @@ export class ReportService{
         }
     }
 
-    static async createReports(data){
+    static async createReports(data, imgPath = ""){
         try {
+            const report = await prisma.report.create({
+                data: data
+            })
+
+            if(report===0) throw new Error("Fallo al crear el reporte");
+
+            console.log(report)
             
+            if(!imgPath) return {message: "Reporte creado exitosamente"}
+
+            await prisma.reportImage.create({
+                data: {
+                    url: imgPath,
+                    reportId: report.id
+                }
+            })
+
+            return {
+                data: report,
+                message: "Reporte creado exitosamente, imagen almaecenada correctaments"
+            }
         } catch (error) {
             
         }
